f8439ebbeedbfeb7d4b3844482433b79d3c4660a
Benten <ben@benten.ca>
2023-07-08 03:37:39 -0600

    cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ class Counter extends Immediate {
   count = 0;
   view(){
     return this.html`
-      <button @click=${() => this.count++}>${this.count}</button>
+      <button @click=${() => this.count++}>The count is ${this.count}</button>
     `
   }
 }
